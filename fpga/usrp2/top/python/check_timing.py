Dantali0n/uhd

commit 699cff59415eae9652d3e628ed7425af99710671

uhd: Surround all open() calls with with-statement
In addition to commit ab8ec9973324299828d48d7a27258939dd6ca837
this adds `with` statements to remaining files in the UHD code base.

fpga/usrp2/top/python/check_timing.py
@@ -22,11 +22,12 @@ def print_timing_constraint_summary(twr_file):
     output = ""
     keep = False
     done = False
-    for line in open(twr_file).readlines():
-        if 'Derived Constraint Report' in line: keep = True
-        if 'constraint' in line and 'met' in line: done = True
-        if not keep and done: keep = True
-        if keep: output += line
+    with open(twr_file) as f:
+        for line in f:
+            if 'Derived Constraint Report' in line: keep = True
+            if 'constraint' in line and 'met' in line: done = True
+            if not keep and done: keep = True
+            if keep: output += line
         if done: break
     print("\n\n"+output)
 

fpga/usrp3/top/python/check_timing.py
@@ -26,11 +26,12 @@ def print_timing_constraint_summary(twr_file):
     except IOError:
         print("cannot open or find %s; no timing summary to print!"%twr_file)
         exit(-1)
-    for line in open(twr_file).readlines():
-        if 'Derived Constraint Report' in line: keep = True
-        if 'constraint' in line and 'met' in line: done = True
-        if not keep and done: keep = True
-        if keep: output += line
+    with open(twr_file) as f:
+        for line in f:
+            if 'Derived Constraint Report' in line: keep = True
+            if 'constraint' in line and 'met' in line: done = True
+            if not keep and done: keep = True
+            if keep: output += line
         if done: break
     print(("\n\n"+output))
 

fpga/usrp2/top/extract_usage.py
@@ -29,10 +29,11 @@ ALL_MAP_FILES = """\
 def extract_map_from_file(path):
     output = ''
     found = False
-    for line in open(path).readlines():
-        if line.strip() == 'Mapping completed.': found = False
-        if line.strip() == 'Logic Utilization:': found = True
-        if found: output += line
+    with open(path) as f:
+        for line in f:
+            if line.strip() == 'Mapping completed.': found = False
+            if line.strip() == 'Logic Utilization:': found = True
+            if found: output += line
     return output
 
 def extract_maps():
@@ -54,5 +55,9 @@ def extract_maps():
 
 if __name__ == '__main__':
     summary = extract_maps()
-    if len(sys.argv) == 1: print(summary)
-    else: open(sys.argv[1], 'w').write(summary)
+    if len(sys.argv) == 1:
+        print(summary)
+    else:
+        with open(sys.argv[1], 'w') as f:
+            f.write(summary)
+

fpga/usrp3/tools/scripts/xil_bitfile_parser.py
@@ -102,7 +102,8 @@ def main():
             print('Bitstream Size: ' + str(header['bitstream_len']))
         # Write a bin file
         if args.bin_out:
-            open(args.bin_out, 'wb').write(flip32(data) if args.flip else data)
+            with open(args.bin_out, 'wb') as f:
+                f.write(flip32(data) if args.flip else data)
 
 if __name__ == '__main__':
     main()

host/lib/convert/gen_convert_general.py
@@ -312,4 +312,6 @@ if __name__ == '__main__':
                 width=width, to_host='uhd::wtohx', to_wire='uhd::htowx',
                 cpu_type=cpu_type, do_scale=do_scale
             )
-    open(sys.argv[1], 'w').write(output)
+
+    with open(sys.argv[1], 'w') as f:
+        f.write(output)

host/lib/deps/pybind11/remove_comments.py
@@ -44,10 +44,11 @@ def main():
         for fname in files:
             path = os.path.join(root, fname)
             print("Modifying {}...".format(path))
-            new_file = comment_remover(open(os.path.join(root, fname)).read())
+            with open(os.path.join(root, fname), 'r') as f:
+                new_file = comment_remover(f.read())
             new_file = remove_trailing_whitespace(new_file)
-            open(os.path.join(root, fname), 'w').write(
-                DEFAULT_HEADER + new_file)
+            with open(os.path.join(root, fname), 'w') as f:
+                f.write(DEFAULT_HEADER + new_file)
 
 if __name__ == "__main__":
     main()

host/lib/transport/gen_vrt_if_packet.py
@@ -349,14 +349,16 @@ def parse_tmpl(_tmpl_text, **kwargs):
 
 if __name__ == '__main__':
     import sys
-    open(sys.argv[1], 'w').write(parse_tmpl(
-        TMPL_TEXT,
-        file=__file__,
-        sid_p = 0b0000001,
-        cid_p = 0b0000010,
-        tsi_p = 0b0000100,
-        tsf_p = 0b0001000,
-        tlr_p = 0b0010000,
-        sob_p = 0b0100000,
-        eob_p = 0b1000000,
-    ))
+
+    with open(sys.argv[1], 'w') as f:
+        f.write(parse_tmpl(
+            TMPL_TEXT,
+            file=__file__,
+            sid_p = 0b0000001,
+            cid_p = 0b0000010,
+            tsi_p = 0b0000100,
+            tsf_p = 0b0001000,
+            tlr_p = 0b0010000,
+            sob_p = 0b0100000,
+            eob_p = 0b1000000,
+        ))

host/utils/usrp2_card_burner.py
@@ -51,8 +51,10 @@ def get_dd_path():
         dd_path = os.path.join(tempfile.gettempdir(), 'dd.exe')
         if not os.path.exists(dd_path):
             print('Downloading dd.exe to %s'%dd_path)
-            dd_bin = urllib.request.urlopen('http://files.ettus.com/dd.exe').read()
-            open(dd_path, 'wb').write(dd_bin)
+            with urllib.request.urlopen('http://files.ettus.com/dd.exe') as response:
+                dd_bin = response.read()
+            with open(dd_path, 'wb') as f:
+                f.write(dd_bin)
         return dd_path
     return 'dd'
 
@@ -143,7 +145,8 @@ def verify_image(image_file, device_file, offset):
     tmp_file = get_tmp_file()
 
     #read the image data
-    img_data = open(image_file, 'rb').read()
+    with open(image_file, 'rb') as f:
+        img_data = f.read()
     count = int_ceil_div(len(img_data), SECTOR_SIZE)
 
     #execute a dd subprocess
@@ -157,7 +160,8 @@ def verify_image(image_file, device_file, offset):
     )
 
     #verfy the data
-    tmp_data = open(tmp_file, 'rb').read(len(img_data))
+    with open(tmp_file, 'rb') as f:
+        tmp_data = f.read(len(img_data))
     if img_data != tmp_data: return 'Verification Failed:\n%s'%verbose
     return 'Verification Passed:\n%s'%verbose
 
@@ -166,11 +170,13 @@ def write_image(image_file, device_file, offset):
     tmp_file = get_tmp_file()
 
     #write the padded image data
-    img_data = open(image_file, 'rb').read()
+    with open(image_file, 'rb') as f:
+        img_data = f.read()
     count = int_ceil_div(len(img_data), SECTOR_SIZE)
     pad_len = SECTOR_SIZE*count - len(img_data)
     padding = bytes(b'\x00')*pad_len #zero-padding
-    open(tmp_file, 'wb').write(img_data + padding)
+    with open(tmp_file, 'wb') as f:
+        f.write(img_data + padding)
 
     #execute a dd subprocess
     verbose = command(

commit d110c060f26a16481cd38bf56c21d46d167bc3f6

fpga: Fix check_timing.py break indent
This fixes an issue introduced by commit 699cff5941, where the break
statement was incorrectly indented outside the for loop.

fpga/usrp2/top/python/check_timing.py
@@ -28,7 +28,7 @@ def print_timing_constraint_summary(twr_file):
             if 'constraint' in line and 'met' in line: done = True
             if not keep and done: keep = True
             if keep: output += line
-        if done: break
+            if done: break
     print("\n\n"+output)
 
 if __name__=='__main__': map(print_timing_constraint_summary, sys.argv[1:])

fpga/usrp3/top/python/check_timing.py
@@ -32,7 +32,7 @@ def print_timing_constraint_summary(twr_file):
             if 'constraint' in line and 'met' in line: done = True
             if not keep and done: keep = True
             if keep: output += line
-        if done: break
+            if done: break
     print(("\n\n"+output))
 
 if __name__=='__main__': list(map(print_timing_constraint_summary, sys.argv[1:]))